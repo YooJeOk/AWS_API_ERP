812cb49a39bf8b6d5f2d63f55a3ce27e8fefba8c
jokwon74 <jokwon777@naver.com>
2024-10-16 10:15:09 +0900

    jhm2 test3

--- FinalProject/src/main/java/com/ERP/FinalProject/FinalProjectApplication.java
+++ FinalProject/src/main/java/com/ERP/FinalProject/FinalProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinalProjectApplication {
-	//해문
+	//해문 123
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectApplication.class, args);
 	}
