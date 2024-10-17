b4cee3b18a91dac9ef66eb90b9eeee3230b08712
YooJeOk <goawpdnr@naver.com>
2024-10-17 19:40:37 +0900

    erp

---
+++ FinalProject/src/main/erp/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// reportWebVitals은 필요 없다면 주석 처리
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// reportWebVitals(); 주석 처리 또는 제거
