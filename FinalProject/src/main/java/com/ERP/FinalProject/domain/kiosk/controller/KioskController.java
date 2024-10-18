3d9329484f1b50f7430ac00c10fbdd01b06c1bef
jokwon74 <jokwon777@naver.com>
2024-10-18 15:43:39 +0900

    merge2
    merge2

--- FinalProject/src/main/java/com/ERP/FinalProject/domain/kiosk/controller/KioskController.java
+++ FinalProject/src/main/java/com/ERP/FinalProject/domain/kiosk/controller/KioskController.java
@@ -21,31 +21,15 @@ public class KioskController {
 	@Autowired
 	private KioskService service;
 	
-
-	@GetMapping("/recommended")
-	public ResponseEntity<Map<String, Object>> kioskmenu(Model model, @RequestParam(defaultValue = "0") int page,
-			@RequestParam(defaultValue = "6") int size) {
-	
-		Pageable pageable = PageRequest.of(page, size);
-		Page<Product> recommendedProducts = kioskservice.getRecommendedProducts(pageable);
-		Page<Coffee> recommendedCoffees = kioskservice.getRecommendedCoffees(pageable);
-
-		List<Object> recommendedItems = new ArrayList<Object>();
-		recommendedItems.addAll(recommendedProducts.getContent());
-		recommendedItems.addAll(recommendedCoffees.getContent());
-
-		int totalElements = (int) (recommendedProducts.getTotalElements() + recommendedCoffees.getTotalElements());
-		int totalPages = (int) Math.ceil((double) totalElements / size);
-
-		System.out.println("totalElements: "+totalElements);
-		System.out.println("recommendedItems: "+recommendedItems);
-		Map<String, Object> response = new HashMap<>();
 		response.put("items", recommendedItems);
-        response.put("currentPage", page);
-        response.put("totalItems", totalElements);
-        response.put("totalPages", totalPages);
-
-        return ResponseEntity.ok(response);
-
+	@GetMapping("/kioskmenu")
+	public String kioskmenu(Model model,
+			@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "추천메뉴") String category) {
+		Pageable pageable = PageRequest.of(page, 6);
+		Page<Product> KioskMenuPage = service.findAll(pageable);
+		log.info("키오스크 메뉴 데이터:" + KioskMenuPage);	
+		//test=ds
+		return "";
 	}
 }
