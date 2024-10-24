67fc015d43d12c2712099db93a2d1743ebcb05da
jokwon74 <70688364+jokwon74@users.noreply.github.com>
2024-10-25 00:09:48 +0900

    키오스크 카트아이템 보내기 진행중
    키오스크 카트아이템 보내기 진행중

---
+++ FinalProject/src/main/java/com/ERP/FinalProject/domain/kiosk/repository/CoffeeOptionSalesDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.ERP.FinalProject.domain.kiosk.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ERP.FinalProject.domain.kiosk.entity.CoffeeOptionSalesDetails;
+
+public interface CoffeeOptionSalesDetailsRepository extends JpaRepository<CoffeeOptionSalesDetails, Integer>{
+
+}
