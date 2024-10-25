0a28ee552a0ff9f38405444af6fb0052446987d7
juncheol001 <wnscjf04@gmail.com>
2024-10-25 17:58:09 +0900

    dd

---
+++ FinalProject/src/main/java/com/ERP/FinalProject/domain/kiosk/entity/SalesDetails.java
@@ -0,0 +1,37 @@
+package com.ERP.FinalProject.domain.kiosk.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "SalesDetails")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SalesDetails {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int detailID;
+
+    private int saleID;
+
+    private int productID;
+
+    private int coffeeID;
+
+    private int quantitySold;
+
+    private int salePrice;
+
+}
---
+++ FinalProject/src/main/java/com/ERP/FinalProject/domain/production/ProductionController.java
@@ -0,0 +1,5 @@
+package com.ERP.FinalProject.domain.production;
+
+public class ProductionController {
+
+}
