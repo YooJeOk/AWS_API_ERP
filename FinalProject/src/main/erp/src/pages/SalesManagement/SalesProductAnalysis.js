07bf0c28c2f28bc245ce319a5ac19575ee54e5ee
juncheol001 <wnscjf04@gmail.com>
2024-10-28 17:49:22 +0900

    55

--- FinalProject/src/main/erp/src/pages/SalesManagement/SalesProductAnalysis.js
+++ FinalProject/src/main/erp/src/pages/SalesManagement/SalesProductAnalysis.js
@@ -2,14 +2,12 @@ import React from 'react';
 import SalesProductAnalysisSearchBar from '../../components/SalesManagement/SalesProductAnalysis/SalesProductAnalysisSearchBar';
 import SalesProductAnalysisTable from '../../components/SalesManagement/SalesProductAnalysis/SalesProductAnalysisTable';
 
-
-
 import { Container, Row, Col } from 'react-bootstrap';
 import '../../css/SalesManagement/SalesRecord.css'
 function SalesProductAnalysis() {
   return (
     <div className="custom-background">
-    
+    s
 
 
       {/* 메인 콘텐츠 */}
