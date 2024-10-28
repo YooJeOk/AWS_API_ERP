6a2c2a277f6c35e623531c31583569211e3f7928
jokwon74 <jokwon777@naver.com>
2024-10-28 14:24:17 +0900

    jhm merge
    FinalProject\src\main\erp\package-lock.json

---
+++ FinalProject/src/main/erp/src/components/KioskManagement/KioskSidebar.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './styles.css';
+
+function KioskSidebar() {
+    const location = useLocation();  // 현재 경로를 가져옴
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);  // 사이드바 상태 관리
+
+    // 경로 변경에 따라 사이드바 열림/닫힘 상태를 변경하는 useEffect
+    useEffect(() => {
+        if (location.pathname === '/') {
+            setIsSidebarOpen(false);  // 메인 페이지에서는 사이드바를 닫음
+        } else {
+            setIsSidebarOpen(true);  // 다른 페이지에서는 사이드바를 염
+        }
+    }, [location]);
+
+    return (
+        <div
+            className="sidebar"
+            style={{
+                position: 'fixed',
+                left: isSidebarOpen ? '0' : '-250px',  // 열리면 0, 닫히면 -250px
+                transition: 'left 0.3s ease',
+                width: '220px',
+                height: '100vh',
+                padding: '10px',
+                zIndex: 2,
+                backgroundColor: '#F6F6F6'
+            }}
+        >
+            <div className="sidebar-menu" >
+            <div className="dropdown">
+                    <button className="menu-item" style={{ backgroundColor: '#F0C490', color: 'white' }}>상품 관리</button>
+                    <ul className="dropdown-menu">
+                        <li><Link to="/KioskSelect">상품 조회</Link></li>
+                        <li><Link to="/KioskSelect">상품 가격 조정</Link></li>
+                        <li><Link to="/KioskSelect">상품 추천여부 설정</Link></li>
+                        <li><Link to="/KioskSelect">상품 삭제</Link></li>
+                    </ul>
+                </div>
+
+                <div className="dropdown">
+                    <Link to="/KioskSelect">
+                        <button className="menu-item" style={{ backgroundColor: '#F0C490', color: 'white' }}>
+                            상품 등록
+                        </button>
+                    </Link>
+                </div>
+
+
+
+
+            </div>
+        </div>
+    );
+}
+
+export default KioskSidebar;
---
+++ FinalProject/src/main/erp/src/pages/KioskManagement/KioskSelect.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const KioskSelect = ()=>{
+
+    return(
+        <div>
+        <h1>KioskSelect Page</h1>
+        <p>test</p>
+      </div>
+    );
+}
+export default KioskSelect;
